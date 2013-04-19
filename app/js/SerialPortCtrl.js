e47fe057696e443c394d3040cf2206797b85a394
ricorx7 <ricorx7@gmail.com>
2013-04-19 16:05:47 -0700

    Updated comments.

--- app/js/SerialPortCtrl.js
+++ app/js/SerialPortCtrl.js
@@ -1,3 +1,42 @@
+/*
+ * Copyright 2011, Rico Studio 
+ * All rights reserved.
+ * http://www.rico-studio.com
+ * https://github.com/ricorx7
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice, this list of
+ *      conditions and the following disclaimer.
+ *      
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *      of conditions and the following disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *      
+ *  THIS SOFTWARE IS PROVIDED BY Rico Studio ''AS IS'' AND ANY EXPRESS OR IMPLIED 
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Rico Studio OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Rico Studio.
+ * 
+ * 
+ * HISTORY
+ * -----------------------------------------------------------------
+ * Date            Initials    Version    Comments
+ * -----------------------------------------------------------------
+ * 04/19/2013      RC          0.0.1           Initial coding
+ * 
+ */
+
 /* Controllers */
 var serialApp = angular.module("SerialPortCtrl", []);
 
@@ -10,11 +49,17 @@ serialApp.factory('SerialPortOptions', function() {
 })
 
 
-
+/*
+ * Get the ports from the serial port.  And read and
+ * write to the serial port.
+ */
 function SerialPortCtrl($scope, SerialPortOptions) {
 
+	// Set the SerialPortOptions and monitor for any changes to the options
 	$scope.serialPortOptions = SerialPortOptions;
 	$scope.$watch( function () { return SerialPortOptions; } , function() { $scope.serialPortOptions = SerialPortOptions; }, true);
+	
+	// Buffer to store the incoming data
 	$scope.buffer = "";
 
 
@@ -31,7 +76,8 @@ function SerialPortCtrl($scope, SerialPortOptions) {
 			// another function.
 			// http://stackoverflow.com/questions/10179488/the-view-is-not-updated-in-angularjs
 			$scope.$apply(function() { SerialPortOptions.ports = ports; });
-	      
+			
+			// Display the port info to the console
 			var portInfo = "";
 			ports.forEach(function (port) {
 				console.log(port.comName);
@@ -49,9 +95,7 @@ function SerialPortCtrl($scope, SerialPortOptions) {
 	
 		//Initialize the serial port
 		var SerialPort = require("serialport").SerialPort
-		var serialPort = new SerialPort("COM14", {
-		baudrate: 115200
-		});
+		var serialPort = new SerialPort("COM14", { baudrate: 115200 });
 
 		//Open the serial data
 		serialPort.open(function () {
--- app/js/SerialPortWriterCtrl.js
+++ app/js/SerialPortWriterCtrl.js
@@ -1,13 +1,66 @@
+/*
+ * Copyright 2011, Rico Studio 
+ * All rights reserved.
+ * http://www.rico-studio.com
+ * https://github.com/ricorx7
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice, this list of
+ *      conditions and the following disclaimer.
+ *      
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *      of conditions and the following disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *      
+ *  THIS SOFTWARE IS PROVIDED BY Rico Studio ''AS IS'' AND ANY EXPRESS OR IMPLIED 
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Rico Studio OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Rico Studio.
+ * 
+ * 
+ * HISTORY
+ * -----------------------------------------------------------------
+ * Date            Initials    Version    Comments
+ * -----------------------------------------------------------------
+ * 04/19/2013      RC          0.0.1           Initial coding
+ * 
+ */
+
+/*
+ * Write Serial Port data to a file.
+ * This will watch the buffer for changes.
+ * If any data is received, the data will be
+ * written to a file.
+ */
 angular.module("SerialPortWriterCtrl", []);
   
 
+/*
+ * Write Serial Port data to a file.
+ * This will watch the buffer for changes.
+ * If any data is received, the data will be
+ * written to a file.
+ * Currently this is an unbuffered write.
+ */
 function SerialPortWriterCtrl($scope, SerialPortOptions) {
 	// Filepath to write the file
 	$scope.folderPath = "C:\\RTI_Node_Capture\\";
 	$scope.fileName = 'test.bin';
 
-	// Buffer from the serial port
-	// This will also set any updates received from the serialport buffer
+	// Monitor the buffer in SerialPortOptions for any changes.
+	// When the buffer from the serial port changes
+	// this will also set any updates received from the serialport buffer
 	$scope.buffer = SerialPortOptions.buffer;
 	$scope.$watch( function () { return SerialPortOptions.buffer; } , 
 		function() { 
